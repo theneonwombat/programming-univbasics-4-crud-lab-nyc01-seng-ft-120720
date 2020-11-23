98045c5f445a443137389ad2d5cf9fbf6efa1e51
Alec Caro <alec.caro@gmail.com>
2020-11-23 01:44:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  [0, 2, "spinich", nil]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
