04e5869ca91bdcd623d4d7f64846d949171862cf
Alec Caro <alec.caro@gmail.com>
2020-11-23 01:47:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.shift(element)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -20,7 +20,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.unshift
+  array.shift
   array
 end
 
